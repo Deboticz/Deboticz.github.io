9c9e26e1320712d8cca1ffe11b30fa41918b25c1
Nino-OS <155100453+Nino-OS@users.noreply.github.com>
2024-07-28 20:41:56 +0200

    v.1.0

--- script.js
+++ script.js
@@ -113,29 +113,3 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
-var options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.8,
-};
-
-var observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach(function (entry) {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("active");
-      observer.unobserve(entry.target);
-    }
-  });
-}, options);
-
-var targets = document.querySelectorAll(".pricing .card");
-targets.forEach(function (target) {
-  observer.observe(target);
-});
-
-//e
-
-var targets = document.querySelectorAll(".card");
-targets.forEach(function (target) {
-  observer.observe(target);
-});
