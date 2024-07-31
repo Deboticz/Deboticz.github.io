96c5319cbea86edbaffb70a1465d4276272b980c
Nino-OS <robotic.vb21@gmail.com>
2024-07-31 17:04:39 +0200

    v.1.4

--- script.js
+++ script.js
@@ -1,62 +1,3 @@
-function openMenu() {
-  const nav = document.querySelector("nav");
-  nav.classList.toggle("open");
-
-  const icon = document.getElementById("menuButton");
-  const old = document.getElementById("old");
-
-  old.style.display = "none";
-
-  if (nav.classList.contains("open")) {
-    icon.classList.remove("ri-menu-fill");
-    icon.classList.add("ri-close-fill");
-  } else {
-    icon.classList.remove("ri-close-fill");
-    icon.classList.add("ri-menu-fill");
-  }
-}
-
-function scrollToElement(elementSelector, instance = 0) {
-  const elements = document.querySelectorAll(elementSelector);
-  if (elements.length > instance) {
-    elements[instance].scrollIntoView({ behavior: "smooth" });
-  }
-}
-
-function offMenü() {
-  const n = document.querySelector("nav");
-  n.classList.toggle("open");
-
-  const i = document.querySelector(".ri-close-fill");
-  i.classList.remove("ri-close-fill");
-  const o = document.getElementById("old");
-  o.style.display = "block";
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const link1 = document.getElementById("informations");
-  const link2 = document.getElementById("hostinginfos");
-  const link3 = document.getElementById("reviews");
-
-  link1.addEventListener("click", (event) => {
-    event.preventDefault();
-    scrollToElement("#skills");
-    offMenü();
-  });
-
-  link2.addEventListener("click", (event) => {
-    event.preventDefault();
-    scrollToElement("#portfolio");
-    offMenü();
-  });
-
-  link3.addEventListener("click", (event) => {
-    event.preventDefault();
-    scrollToElement(".feedback");
-    offMenü();
-  });
-});
-
 document.addEventListener("contextmenu", function (event) {
   event.preventDefault();
 });
@@ -116,6 +57,67 @@ document.addEventListener("keydown", function (event) {
     event.preventDefault();
   }
 });
+
+function openMenu() {
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("open");
+
+  const icon = document.getElementById("menuButton");
+  const old = document.getElementById("old");
+
+  old.style.display = "none";
+
+  if (nav.classList.contains("open")) {
+    icon.classList.remove("ri-menu-fill");
+    icon.classList.add("ri-close-fill");
+  } else {
+    icon.classList.remove("ri-close-fill");
+    icon.classList.add("ri-menu-fill");
+  }
+}
+
+function scrollToElement(elementSelector, instance = 0) {
+  const elements = document.querySelectorAll(elementSelector);
+  if (elements.length > instance) {
+    elements[instance].scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+function offMenü() {
+  const n = document.querySelector("nav");
+  n.classList.toggle("open");
+
+  const i = document.querySelector(".ri-close-fill");
+  i.classList.remove("ri-close-fill");
+  const o = document.getElementById("old");
+  o.style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const link1 = document.getElementById("informations");
+  const link2 = document.getElementById("hostinginfos");
+  const link3 = document.getElementById("reviews");
+
+  link1.addEventListener("click", (event) => {
+    event.preventDefault();
+    scrollToElement("#skills");
+    offMenü();
+  });
+
+  link2.addEventListener("click", (event) => {
+    event.preventDefault();
+    scrollToElement("#portfolio");
+    offMenü();
+  });
+
+  link3.addEventListener("click", (event) => {
+    event.preventDefault();
+    scrollToElement(".feedback");
+    offMenü();
+  });
+});
+
+
 <<<<<<< HEAD
 =======
 
